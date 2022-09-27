6993f1f75870f64d32fa3fdc16afda76fac8e2b7
istrav <travis.burandt@gmail.com>
2022-09-27 14:22:52 -0500

    description

--- database/schema/ideas.ts
+++ database/schema/ideas.ts
@@ -112,7 +112,7 @@ export default {
       }
     },
   },
-  required: ['id', 'slug', 'name', 'namespace'],
+  required: ['id', 'slug', 'name', 'description', 'namespace'],
   indexes: [
     ['slug', 'namespace']
   ]
